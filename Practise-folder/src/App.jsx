4fd2c97e55f7e511f451bc4ee4f62a3d19ffbb7e
Animesh Gandhi <92109865+gxndh1@users.noreply.github.com>
2026-06-12 12:09:59 +0530

    Add files via upload

---
+++ Practise-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Counter from "./Counter"
+
+const App = () =>{
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  )
+}
+export default App
